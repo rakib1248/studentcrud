70b2bf18b903eabe36f2ba29200f34cf21a43516
rakib1248 <rakib25059159@gmail.com>
2025-10-29 11:56:54 +0300

    update All  file

--- src/utils/axiosApi.js
+++ src/utils/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://studentnestapi.onrender.com",
+  baseURL: "https://domorstudent.onrender.com/",
   timeout: "20000",
   withCredentials: false,
 });
